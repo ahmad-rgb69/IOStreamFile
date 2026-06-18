792029f36e5a60f6c317e72f29664ddb30253474
Ahmad Aldy <aldywalker123@gmail.com>
2026-06-18 21:52:17 +0800

    Selesai Bagian 2 : Baca Tulis Berkas

---
+++ src/bagian2/bacatulis/LatihanMandiriBagian2MenambahData.java
@@ -0,0 +1,33 @@
+
+package bagian2.bacatulis;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class LatihanMandiriBagian2MenambahData {
+    public static void main(String[] args) {
+        try (PrintWriter penulis = new PrintWriter(new FileWriter("hari.txt", true))){
+            penulis.println("Sabtu");
+            penulis.println("Minggu");
+            System.out.println("=============================================");
+            System.out.println("Dua nama hari baru ditambahkan tanpa menghapus data lama.");
+            System.out.println("\n=============================================");
+        } catch (IOException e){
+            System.out.println("Gagal menambah data : "+ e.getMessage());
+        }
+        
+        try(BufferedReader pembaca = new BufferedReader(new FileReader("hari.txt"))){
+            String baris;
+            int nomor = 1;
+            while ((baris = pembaca.readLine()) != null){
+                System.out.println(nomor + ". " + baris);
+                nomor++;
+            } 
+        }catch (IOException e){
+                    System.out.println("Gagal membaca : " + e.getMessage());
+        }
+    }
+}
